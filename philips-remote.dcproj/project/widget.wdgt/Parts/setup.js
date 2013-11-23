9b34ba1e97fdfe6897cf458d01b35b4ee4588b60
Ilya Tkachuk <tkachuk.i.m@gmail.com>
2013-11-23 03:27:57 +0200

    Added simple numeric and color buttons UI

--- philips-remote.dcproj/project/widget.wdgt/Parts/setup.js
+++ philips-remote.dcproj/project/widget.wdgt/Parts/setup.js
@@ -5,11 +5,15 @@
  code generator otherwise the changes will be lost.
  */
 var dashcodePartSpecs = {
-    "cellImage": { "view": "DC.ImageLayout" },
-    "cellLabel": { "text": "Item", "view": "DC.Text" },
+    "cellLabel": { "view": "DC.Text" },
+    "cellLabel2": { "view": "DC.Text" },
     "done": { "creationFunction": "CreateGlassButton", "onclick": "showFront", "text": "Done" },
-    "grid": { "allowsEmptySelection": true, "dataArray": ["Item 3"], "dataSourceName": "numericbuttons", "labelElementId": "cellLabel", "sampleRows": 1, "selectionEnabled": true, "useDataSource": true, "view": "DC.List" },
+    "gridColors": { "allowsEmptySelection": true, "dataArray": ["Item 3"], "dataSourceName": "colorButtons", "labelElementId": "cellLabel2", "sampleRows": 4, "selectionEnabled": true, "useDataSource": true, "view": "DC.List" },
+    "gridNumerics": { "allowsEmptySelection": true, "dataArray": ["Item 3"], "dataSourceName": "numericButtons", "labelElementId": "cellLabel", "sampleRows": 4, "selectionEnabled": true, "useDataSource": true, "view": "DC.List" },
     "helloText": { "text": "Hello, World!", "view": "DC.Text" },
+    "imageColors": { "view": "DC.ImageLayout" },
+    "imageNumerics": { "view": "DC.ImageLayout" },
     "info": { "backgroundStyle": "black", "creationFunction": "CreateInfoButton", "foregroundStyle": "white", "frontID": "front", "onclick": "showBack" }
 };
+
 
